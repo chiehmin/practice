bb15e44552cfc96e45b6f5e38f13eb88d8ecb3b9
FatMinMin <chiehmin18@gmail.com>
2017-03-24 00:11:54 +0800

    add poll.c

---
+++ system/poll.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/poll.h>
+
+#define TIMEOUT 5
+
+int main (void)
+{
+	struct pollfd fds[2];
+	int ret;
+
+	/* watch stdin for input */
+	/* echo fatminmin | ./poll will make stdin to be readable*/
+	/* or you can type something when ./poll is executing */
+	fds[0].fd = STDIN_FILENO;
+	fds[0].events = POLLIN;
+
+	/* watch stdout for ability to write */
+	/* stdout is always writable */
+	fds[1].fd = STDOUT_FILENO;
+	/* fds[1].events = POLLOUT; */
+
+	ret = poll(fds, 2, TIMEOUT * 1000);
+
+	if (ret == -1) {
+		perror ("poll");
+		return 1;
+	}
+
+	if (!ret) {
+		printf ("%d seconds elapsed.\n", TIMEOUT);
+		return 0;
+	}
+
+	if (fds[0].revents & POLLIN) {
+		char buf[256] = {};
+
+		printf ("stdin is readable\n");
+		fgets(buf, 256, stdin);
+		printf ("from stdin: %s\n", buf);
+	}
+
+
+	if (fds[1].revents & POLLOUT)
+		printf ("stdout is writable\n");
+
+	return 0;
+
+}

92b3c6cf18b3a1752e62cc176b020a897c4d6bbb
FatMinMin <chiehmin18@gmail.com>
2017-05-03 01:40:46 +0800

    add rvalue test

---
+++ c_cpp/rvalue.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void test(string &param)
+{
+	cout << param << endl;
+}
+
+void test2(string &&param)
+{
+	cout << '\t' << param << endl;
+	param[0] = 'g';
+	cout << '\t' << param << endl;
+}
+
+
+int main()
+{
+	/* test("haha"); // compile error */
+	test2("haha"); // ok
+
+	string x = "minmin";
+	test(x); // ok
+	/* test2(x); // compile error */
+	test2(move(x)); // ok
+	cout << x << endl;
+}
